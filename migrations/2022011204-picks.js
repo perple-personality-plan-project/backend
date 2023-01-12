0df3d8fd600d78a39ec636ffca94cefe846e7d58
kimheeseok <kim1140129@gmail.com>
2023-01-12 23:23:23 +0900

    <FIX> : Migration 파일명 변경 npx sequelize-cli db:migrate 명령어시 에러 발생으로 파일명 변경

--- migrations/2022011204-picks.js
+++ migrations/2022011204-picks.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('maps', {
+    await queryInterface.createTable('picks', {
       pick_id: {
         allowNull: false,
         autoIncrement: true,
@@ -42,6 +42,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('maps');
+    await queryInterface.dropTable('picks');
   },
 };
