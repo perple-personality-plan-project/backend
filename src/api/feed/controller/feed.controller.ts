7feb8d5face61828998e81460d98a7c8c8518df2
Yoseb Han <yosebseb91@gmail.com>
2023-01-26 01:08:02 +0900

    <FIX> : 피드, 코멘트 스웨거 제거

--- src/api/feed/controller/feed.controller.ts
+++ src/api/feed/controller/feed.controller.ts
@@ -18,47 +18,21 @@ import { GlobalResponseInterceptor } from 'src/common/interceptors/global.respon
 import { GlobalExceptionFilter } from '../../../common/filter/global.exception.filter';
 import { FeedRequestDto } from '../dto/feed.request.dto';
 import { FeedService } from '../service/feed.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { request } from 'http';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { AuthGuard } from '@nestjs/passport';
 import { PositiveIntPipe } from '../../../common/pipes/positiveInt.pipe';
 
 @Controller('feed')
-@ApiTags('feed')
 @UseInterceptors(GlobalResponseInterceptor)
 @UseFilters(GlobalExceptionFilter)
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
-  @ApiOperation({ summary: 'mbti별 피드 조회' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
   @Get('/search')
   async getFeedMbti(@Query('mbti') mbti: string) {
     return this.feedService.getFeedMbti(mbti);
   }
 
-  @ApiOperation({ summary: '피드 생성' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-    type: FeedRequestDto,
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
-  @ApiResponse({
-    status: 412,
-    description: 'description는 필수값 입니다',
-  })
   @UseGuards(AuthGuard('jwt'))
   @Post()
   @UseInterceptors(FilesInterceptor('thumbnail', 5))
@@ -72,43 +46,16 @@ export class FeedController {
     return this.feedService.createFeed(body, user_id, files);
   }
 
-  @ApiOperation({ summary: '전체 피드 조회' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
   @Get()
   getAllFeed() {
     return this.feedService.getAllFeed();
   }
 
-  @ApiOperation({ summary: '피드 상세조회' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
   @Get('/:feed_id')
   findFeedById(@Param('feed_id') feed_id) {
     return this.feedService.findFeedById(feed_id);
   }
 
-  @ApiOperation({ summary: '피드 삭제' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
   @UseGuards(AuthGuard('jwt'))
   @Delete('/:feed_id')
   deleteFeed(@Param('feed_id') feed_id, @Req() req) {
@@ -116,15 +63,6 @@ export class FeedController {
     return this.feedService.deleteFeed(feed_id, user_id);
   }
 
-  @ApiOperation({ summary: '피드 좋아요' })
-  @ApiResponse({
-    status: 200,
-    description: '성공!',
-  })
-  @ApiResponse({
-    status: 500,
-    description: 'Server Error...',
-  })
   @UseGuards(AuthGuard('jwt'))
   @Put('/:feed_id/like')
   async createFeedLike(@Param('feed_id') feed_id, @Req() req) {
@@ -136,6 +74,7 @@ export class FeedController {
     }
     return '좋아요 했습니다.';
   }
+
   @UseGuards(AuthGuard('jwt'))
   @Get('/:feed_id/like')
   async getLikeCheck(
